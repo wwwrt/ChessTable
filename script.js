c35b0a0473f9916fd25919e4cd1adfcd5a1692c9
Timo <tpripon99@gmail.com>
2023-05-04 22:09:35 +0300

    pawns(for)

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function setup() {
     createCanvas(480, 480);
+    for (let i = 0; i < 16; i++) {
+        // creem 16 fețe zâmbitoare cu poziții aleatoare
+        let smileyFace = new SmileyFace(floor(random(8)), floor(random(8)));
+        smileyFaces.push(smileyFace);
+      }
 }
